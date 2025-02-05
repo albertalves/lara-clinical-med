2c0e5c94595f1407e1542f09b69516664f4d7bfd
Albert Alves <albertgrillo95@gmail.com>
2025-02-05 19:21:51 -0300

    chore: install swagger

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        L5Swagger\L5SwaggerServiceProvider::class,
     ],
 
     /*
